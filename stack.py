7efe74ff9a5017edecca194a6f6eeecd38d345ab
0komugi <69564461+0komugi@users.noreply.github.com>
2020-11-20 20:50:31 +0800

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,21 +1,33 @@
 
 Class Stack(object):
-  # 创建栈
-  def __init__(self,limit=10):
-    self.stack = []
-    self.limit = limit
+    # 创建栈
+    def __init__(self,limit=10):
+        self.stack = []
+        self.limit = limit
     
-  # 进栈
-  def push(self,data):
-    if len(self.stack) >= self.limit:
-      raise IndexError("超出栈的容量！")
-    self.stack.append(data)
+    # 进栈
+    def push(self,data):
+        if len(self.stack) >= self.limit:
+              raise IndexError("超出栈的容量！")
+        self.stack.append(data)
     
     # 出栈
     def pop(self):
-      if self.stack:
-        return self.stack。pop()
-      else:
-        raise IndexError("这是一个空栈！")
-
+        if self.stack:
+            return self.stack。pop()
+        else:
+            raise IndexError("这是一个空栈！")
     
+    # 返回栈的大小
+    def size(self):
+        return len(self.stack)
+    
+    # 判断栈是否为空
+    def is_empty(self):
+        return not bool(self.stack)
+      
+    # 查看栈顶元素
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+      
