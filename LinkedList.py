e37bdbfc1a05d7ab0ba015cb75b1e0694d9583fa
0komugi <69564461+0komugi@users.noreply.github.com>
2020-11-25 14:22:37 +0800

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,26 @@
+Class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+               
+Class LinkedList:   
+    def __init__(self,head=None):
+        self.head = head
+        
+    def insert(self):    
+
+        
+    def remove(self):    
+        
+        
+    def length    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
